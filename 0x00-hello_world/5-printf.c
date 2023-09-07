dbbf1202db0ca8120e800459ce7c5f00be9fd99d
ScottShadow <rudaseswascottmc@gmail.com>
2023-09-07 13:11:02 +0000

    added 5_printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+* main - Entry Point
+*
+* Description: This Program prints some text on the screen
+*
+* Return: Always 0 (Success)
+*/
+#include <stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
