8832b226ac20dbbb590c8eb6c5162974549a79b9
ScottShadow <rudaseswascottmc@gmail.com>
2023-09-07 14:25:10 +0000

    added 101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,22 @@
+/**
+* main - Entry Point
+*
+* Description: This program prints out some text
+*
+* Return: 1 Always
+*/
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(void)
+{
+	int stlength;
+	char sentence[] = "and that piece of art is useful\" \- /
+Dora Korpar, 2015\-10\-19";
+
+	strlenght = strlen(sentence);
+	write(1, sentece, stlength);
+
+	return (1);
+}
