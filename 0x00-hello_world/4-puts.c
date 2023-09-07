8364127282e1d869c7e17e65008a127c5b9552fa
ScottShadow <rudaseswascottmc@gmail.com>
2023-09-07 13:03:05 +0000

    added 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/**
+ * main - Entry point
+ *
+ * Description: This program prints out some text
+ *
+ * Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0); 
+}
