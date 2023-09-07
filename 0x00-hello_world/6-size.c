13c9654bd938fe6234c1ae8b9b68ec3f7e440442
ScottShadow <rudaseswascottmc@gmail.com>
2023-09-07 13:33:56 +0000

    added 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+/**
+* main - Entry Point
+*
+* Description: Prints Data type size on different computers
+*
+* Return: 0 Always
+*/
+#include <stdio.h>
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of a int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+}
