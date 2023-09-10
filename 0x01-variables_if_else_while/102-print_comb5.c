c77b35ebd73590ca3b22d778af80e4224d0bb86f
ScottShadow <rudaseswascottmc@gmail.com>
2023-09-10 12:49:13 +0000

    fixes

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,18 +6,30 @@
  */
 int main(void)
 {
-	int first, second;
+	int first, second, third, fourth;
 
 	for (first = 48; first < 58; first++)
 	{
 		for (second = 48; second < 58; second++)
 		{
-
-			putchar((char)first);
-			putchar((char)second);
-			putchar(' ');
-			putchar(',');
-			putchar(' ');
+			for (third = first; third < 58; third++)
+			{
+				for (fourth = second + 1; fourth < 58; fourth++)
+				{
+					putchar((char)(first));
+					putchar((char)(second));
+					putchar(' ');
+					putchar((char)(third));
+					putchar((char)(fourth));
+					if (first == 57 && second == 56 && third == 57 && fourth == 57)
+						putchar('\n');
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
 		}
 	}
 	return (0);
