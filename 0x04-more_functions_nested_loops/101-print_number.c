829cf7d0d60744154ced7619c5bf4e8605544a85
ScottShadow <rudaseswascottmc@gmail.com>
2023-09-14 19:49:28 +0000

    fixes

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,23 +7,24 @@
 void print_number(int n)
 {
 	unsigned int tmp = 1000000000;
-	unsigned int x, t2;
+	unsigned int n2, x, t2;
 
+	n2 = n;
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	x = n;
+	x = n2;
 
 	while (tmp >= 10)
 	{
-		if ((unsigned)n >= tmp)
+		if (n2 >= tmp)
 		{
-			_putchar('0' + (n / tmp));
+			_putchar('0' + (n2 / tmp));
 			t2 = tmp;
 
-			while ((n % t2) / (t2 / 10) == 0 && n > 9)
+			while ((n2 % t2) / (t2 / 10) == 0 && n2 > 9)
 			{
 				if (t2 == 10)
 					break;
@@ -31,7 +32,7 @@ void print_number(int n)
 				t2 = t2 / 10;
 			}
 
-			n %= tmp;
+			n2 %= tmp;
 		}
 
 		tmp = tmp / 10;
