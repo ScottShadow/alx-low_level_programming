25997b193959d138a967168897d253e5400a78b7
ScottShadow <rudaseswascottmc@gmail.com>
2023-09-20 16:53:28 +0000

    fixes

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,24 +9,24 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int size = 0;
+	int size = 0;
 
-    while (*dest != '\0')
-    {
-        /* printf("counted dest++\n"); */
-        size++;
-        dest++;
-    }
-    /* printf("size of source : %d\n", size); */
-    while (*src != '\0')
-    {
-        /* printf("set %c to %c\n", *dest, *src); */
-        *dest = *src;
-        dest++;
-        src++;
-        size++;
-    }
+	while (*dest != '\0')
+	{
+		/* printf("counted dest++\n"); */
+		size++;
+		dest++;
+	}
+		/* printf("size of source : %d\n", size); */
+	while (*src != '\0')
+	{
+		/* printf("set %c to %c\n", *dest, *src); */
+		*dest = *src;
+		dest++;
+		src++;
+		size++;
+	}
 
-    *dest = '\0'; /* end the string with a null terminator */
-    return (dest - size);
+	*dest = '\0'; /* end the string with a null terminator */
+	return (dest - size);
 }
