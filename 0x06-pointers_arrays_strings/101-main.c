65c3c82b1ddf7982b8db8d16c40e73fd2599b64f
ScottShadow <rudaseswascottmc@gmail.com>
2023-09-21 13:45:16 +0000

    101 recurse

--- 0x06-pointers_arrays_strings/101-main.c
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -17,7 +17,7 @@ int main(void)
 	_putchar('\n');
 	print_number(-98);
 	_putchar('\n');
-	print_number(-90200800);
+	print_number(-__INT_MAX__);
 	_putchar('\n');
 	return (0);
 }
--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,6 +5,7 @@
  */
 void print_number(int n)
 {
+	unsigned int b = n;
 	if (n < 0)
 	{
 		_putchar('-');
@@ -12,5 +13,5 @@ void print_number(int n)
 	}
 	if (n > 9) /*recurse if the input is 10 or greater*/
 		print_number(n / 10);
-	_putchar('0' + ((unsigned int)n % 10));
+	_putchar('0' + (b % 10));
 }
