8f4a9ecd099023f614fd238eab25f19d8f2f553e
ScottShadow <rudaseswascottmc@gmail.com>
2023-09-21 12:44:34 +0000

    100fix

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,14 +11,14 @@ char *rot13(char *c)
 
 	while (*c != '\0')
 	{
+		encrypt = 13;
 		/* while instead of if, because requirements allow use of only one if*/
 		while ((*c >= 'a' && *c <= 'z') || (*c >= 'A' && *c <= 'Z'))
 		{
 
 			if ((*c >= 'n' && *c <= 'z') || (*c >= 'N' && *c <= 'Z'))
 				encrypt = -13;
-			else
-				encrypt = 13;
+
 			*c = *c + encrypt;
 			break;
 		}
