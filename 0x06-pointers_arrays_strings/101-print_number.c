cb39d2b349479d142b490fbadc8083e6c3d41a29
ScottShadow <rudaseswascottmc@gmail.com>
2023-09-21 13:07:26 +0000

    more fixes on 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,5 +12,5 @@ void print_number(int n)
 	}
 	if (n > 9) /*recurse if the input is 10 or greater*/
 		print_number(n / 10);
-	_putchar('0' + (unsigned)(n % 10));
+	_putchar('0' + (unsigned int)(n % 10));
 }
