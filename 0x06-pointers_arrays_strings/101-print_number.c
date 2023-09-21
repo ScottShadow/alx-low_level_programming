78476553db4f0e7f76dad6d2b609b7f6f711e2ab
ScottShadow <rudaseswascottmc@gmail.com>
2023-09-21 12:58:14 +0000

    more fixes

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,18 +5,12 @@
  */
 void print_number(int n)
 {
-	int output;
-	int input = n;
-
 	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
-		input = n;
 	}
-	output = n % 10;
-	n = n / 10;
-	if (input > 9) /*recurse if the input is 10 or greater*/
-		print_number(n);
-	_putchar('0' + output);
+	if (n > 9) /*recurse if the input is 10 or greater*/
+		print_number(n / 10);
+	_putchar('0' + n % 10);
 }
