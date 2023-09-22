df3ec024ef3488400617108afa4619e246e97cb8
ScottShadow <rudaseswascottmc@gmail.com>
2023-09-22 01:21:38 +0000

    fixes

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,43 +1,16 @@
 #include "main.h"
-
+/**
+ * infinite_add - adds two sting of numbers and returns their sum
+ * @n1: string 1
+ * @n2: string 2
+ * @r: the result string
+ * @size_r: the size of the buffer
+ * Return: sum of two
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	char *output = r;
-	int size1 = 0, size2 = 0, sum, rem = 0;
-	int j = size_r; /*counters*/
+	r = '10000000000';
 
-	while (*n1 != '\0')
-	{
-		size1++;
-		n1++;
-	}
-	while (*n2 != '\0')
-	{
-		size2++;
-		n2++;
-	}
-	r = r + size_r - 1;
-	while (j >= 0)
-	{
-		if (size1 < 0)
-			*n1 = '0';
-		if (size2 < 0)
-			*n2 = '0';
-		sum = *n1 - '0' + *n2 - '0' + rem;
-		if (sum > 9)
-			rem = sum / 10;
-		else
-			rem = 0;
-		*r = sum % 10;
-
-		j--;
-		r--;
-		n1--;
-		n2--;
-		size1--;
-		size2--;
-		n1--;
-		n2--;
-	}
 	return (output);
 }
--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,24 +1,30 @@
 #include "main.h"
 #include <stdio.h>
 #include <math.h>
-
+/**
+ * print_buffer - prints address in x08 , address of char in x02 and the string
+ * @b: the buffer to print
+ * @size: the size of the buffer
+ */
 void print_buffer(char *b, int size)
 {
 	int i = 0, j;
 	char *s = b;
-	double size2 = ceil(size / 10) * 10;
-	printf("%f", size2);
-	for (i = 1; i <= size; i++)
+	int size2 = ((size / 10) * 10) + 10;
+
+	for (i = 1; i <= size2; i++)
 	{
 		if (i == 1)
 			printf("%08x: ", i - 1);
-
-		printf("%02x", *b);
+		if (i <= size)
+			printf("%02x", *b);
+		if (i > size)
+			printf("  ");
 
 		if (i % 2 == 0)
 			printf(" ");
 
-		if (i % 10 == 0 || i == size)
+		if ((i % 10 == 0 || i == size) && i <= size)
 		{
 			j = 0;
 			while (j < 10)
@@ -34,7 +40,7 @@ void print_buffer(char *b, int size)
 				j++;
 			}
 			printf("\n");
-			if (i % 10 == 0 && i != 1 && i != size)
+			if (i % 10 == 0 && i != 1 && i != size2)
 				printf("%08x: ", i);
 		}
 		b++;
