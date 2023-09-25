d6d11465bb07ef9aa52467d0250bc80c63e0f8a8
ScottShadow <rudaseswascottmc@gmail.com>
2023-09-25 16:51:55 +0000

    added 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - sets string s to to
+ * @s: the string to copy
+ * @to: the destination string
+ */
+void set_string(char **s, char *to)
+{
+	to = *s;
+	to++;
+}
