19e6c284d86120abaf5942bfb1e8a77eb2cbb326
ScottShadow <rudaseswascottmc@gmail.com>
2023-09-27 21:43:45 +0000

    fixes on 101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * movetodot - moves pointer to '.'
+ * @s: the pointer to the string
+ * Return: pointer pointing to '.'
+ */
 char *movetodot(char *s)
 {
 	if (*s == '\0')
@@ -7,6 +12,12 @@ char *movetodot(char *s)
 		return (s);
 	return (movetodot(++s));
 }
+/**
+ * wildcheck - compares string with wildcards
+ * @s1: string to compare
+ * @s2: string to compare to s1
+ * Return: 1(Success) strings match, else 0(Fail)
+ */
 int wildcheck(char *s1, char *s2)
 {
 	if (*s2 == '\0')
@@ -15,6 +26,13 @@ int wildcheck(char *s1, char *s2)
 		s1 = movetodot(s1);
 	return (wildcmp(s1, s2));
 }
+/**
+ * wildcmp - main sting compare function
+ * compares s1 to s2
+ * @s1: string to compare
+ * @s2: string to compare to
+ * Return: 1(Success) strings match, else 0(Fail)
+ */
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
