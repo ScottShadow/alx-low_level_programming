c44a76942ce094ccc23ec2f09ef147c69856c847
ScottShadow <rudaseswascottmc@gmail.com>
2023-10-05 15:25:11 +0000

    fixes

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,39 +2,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *  _memset - fill memory with a constant byte
- *
- *	@s:  A pointer to the memory block to be filled.
- *	@b: value: The value to be set. It's an integer but
- *	typically treated as an unsigned char or a byte.
- *	@n: The number of bytes to be set to the specified value.
- *	Return: returns a pointer to the memory area s.
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-	return (s);
-}
-
-/**
  * _calloc - allocates memory for an array, using malloc
- * @nmemb: the size of the array
- * @size: the type size of members of the array
- * Return: pointer with zero initialized
+ * @nmemb: size of the array
+ * @size: the size type of members
+ * Return: pointer to newly allocated memoblock
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p = malloc(size * nmemb);
-
-	if (p == NULL)
-		return (NULL);
 	if ((size == 0) && !nmemb)
 		return (NULL);
-	_memset(p, 0, size);
-	return (p);
+	return ((void *)malloc(size * nmemb));
 }
