4f59cac803977cc76db92c4acfdfa802d5b9dda2
ScottShadow <rudaseswascottmc@gmail.com>
2023-10-05 15:36:49 +0000

    changed data type from void to int

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,26 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
-/**
- *  _memset - fill memory with a constant byte
- *
- *	@s:  A pointer to the memory block to be filled.
- *	@b: value: The value to be set. It's an integer but
- *	typically treated as an unsigned char or a byte.
- *	@n: The number of bytes to be set to the specified value.
- *	Return: returns a pointer to the memory area s.
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-	return (s);
-}
-
 /**
  * _calloc - allocates memory for an array, using malloc
  * @nmemb: the size of the array
@@ -29,12 +8,16 @@ char *_memset(char *s, char b, unsigned int n)
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p = malloc(size * nmemb);
+	int *p = malloc(size * nmemb);
+	unsigned int i;
 
 	if (p == NULL)
 		return (NULL);
 	if ((size == 0) && !nmemb)
 		return (NULL);
-	_memset(p, 0, size);
-	return ((void *)p);
+	for (i = 0; i < size; i++)
+	{
+		p[i] = 0;
+	}
+	return (p);
 }
