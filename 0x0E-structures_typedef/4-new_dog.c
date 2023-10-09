62228368968230ca8ed6f2ae987076c18b57d04f
ScottShadow <rudaseswascottmc@gmail.com>
2023-10-09 16:39:28 +0000

    allocated memory before assigning

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,8 +17,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_p = malloc(sizeof(struct dog));
 	if (dog_p == NULL)
 		return (NULL);
+	dog_p->name = malloc(sizeof(char) * (strlen(name) + 1));
+	if (dog_p->name == NULL)
+	{
+		free(dog_p);
+		return (NULL);
+	}
 	dog_p->name = name;
+
 	dog_p->age = age;
+
+	dog_p->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (dog_p->owner == NULL)
+	{
+		free(dog_p);
+		free(dog_p->name);
+		return (NULL);
+	}
 	dog_p->owner = owner;
 	return (dog_p);
 }
