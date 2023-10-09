ddc2cab5d68c026c85743fb2ecdd3bebb0a31810
ScottShadow <rudaseswascottmc@gmail.com>
2023-10-09 15:40:53 +0000

    added memo check

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * init_dog - initializes man's best friend
  * @d: the dog object
@@ -8,6 +9,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
