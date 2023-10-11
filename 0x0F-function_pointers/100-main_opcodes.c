cd60d56e0a3ff9ff8ea9f89a4f3991da255c6e31
ScottShadow <rudaseswascottmc@gmail.com>
2023-10-11 14:02:34 +0000

    added memo checks

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - program that prints the opcodes of its own main function.
+ * @argc: the number of args passed
+ * @argv: the array with args passed
+ * Return: the 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 	int (*p)(int, char *[]) = main;
 	unsigned int i, count = 0;
+
+	if (p == NULL)
+		return (1);
 	unsigned char *ptr_opcode = (unsigned char *)p;
 
+	if (ptr_opcode == NULL)
+		return (1);
 	if (argc != 2)
 	{
 		printf("Error\n");
