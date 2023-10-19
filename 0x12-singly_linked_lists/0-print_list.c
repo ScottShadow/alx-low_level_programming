0b0559b92da8edf3cb5bc98a951a0495e322e316
ScottShadow <rudaseswascottmc@gmail.com>
2023-10-19 11:29:47 +0000

    new innitial commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "list.h"
+/**
+ * print_list - Prints a singly linked list
+ * @h: the address of the head of the list
+ * Return: The number of items in the list
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 0;
+	const list_t *temp = h;
+	unsigned int length;
+	char *string;
+
+	while (temp != NULL)
+	{
+		string = temp->str;
+		length = temp->len;
+		if (string == NULL)
+		{
+			string = "(nil)";
+			length = 0;
+		}
+		printf("[%d] %s\n", length, string);
+		count++;
+		temp = temp->next;
+	}
+	return (count);
+}
