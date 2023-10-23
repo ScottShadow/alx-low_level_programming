2358ddbce09b1f9885cba66a0c4f0e66c310ffc2
ScottShadow <rudaseswascottmc@gmail.com>
2023-10-23 19:32:45 +0000

    initial commit

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * pop_listint - frees the header of the list
+ * @head: address for the head node
+ * Return: value contained in the removed head node
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *p = *head;
+
+	if (*head == NULL)
+		return (0);
+
+	(*head) = (*head)->next;
+	free(p);
+	return ((*head)->n);
+}
