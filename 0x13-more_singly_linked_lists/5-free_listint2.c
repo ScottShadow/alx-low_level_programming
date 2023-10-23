51453ed752750e0aed4ecc1ef88291fad32138b5
ScottShadow <rudaseswascottmc@gmail.com>
2023-10-23 19:50:06 +0000

    back to free head

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,14 +7,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *p;
 
-	if (*head == NULL)
-		return;
-
 	while (*head != NULL)
 	{
 		p = *head;
 		*head = (*head)->next;
 		free(p);
 	}
-	*head = NULL;
+	free(*head);
 }
