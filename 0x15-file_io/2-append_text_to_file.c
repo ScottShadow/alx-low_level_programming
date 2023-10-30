51eeece91346417b4ae5236d4027cf92d3030975
ScottShadow <rudaseswascottmc@gmail.com>
2023-10-30 18:56:11 +0000

    fixed closed texts

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,10 +10,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	int file_desc;
 	ssize_t written_char;
 
-	if (text_content == NULL)
-		return (1);
+	if (text_content == NULL || filename == NULL)
+		return (-1);
 	file_desc = open(filename, O_RDWR | O_APPEND);
-	if (file_desc == -1 || filename == NULL)
+	if (file_desc == -1)
 		return (-1);
 
 	written_char = write(file_desc, text_content, strlen(text_content));
