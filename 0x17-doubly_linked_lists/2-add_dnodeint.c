2f8ace903936197bf1ad9a12fafa963810a50037
ScottShadow <rudaseswascottmc@gmail.com>
2023-12-08 01:33:25 +0000

    2.6

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,8 +17,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 		newNode->n = n;
 		newNode->next = *head;
+		if (*head)
+			(*head)->prev = newNode;
+
 		*head = newNode;
-		newNode->prev = NULL;
 		return (newNode);
 	}
 	return (NULL);
