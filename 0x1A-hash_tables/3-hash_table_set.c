c0bd5b4e321a070c141bf016a7db0e38792ed149
ScottShadow <rudaseswascottmc@gmail.com>
2023-12-22 23:50:24 +0000

    fix on 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,6 +13,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *newNode;
 	int key_idx;
 
+	if (ht == NULL || key == NULL)
+		return (0);
 	newNode = malloc(sizeof(hash_node_t));
 	if (newNode == NULL)
 		return (0);
