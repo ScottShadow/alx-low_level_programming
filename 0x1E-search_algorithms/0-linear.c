76cd42f2d4b4c1068d7f22f1507e77c744cff502
Scott Rudaseswa <rudaseswascottmc@gmail.com>
2024-04-10 11:42:29 +0200

    codestyle changes

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,11 @@
 #include "search_algos.h"
+/**
+ * linear_search - Searches for a value in an array using linear search.
+ * @array: Pointer to the array to search.
+ * @size: Size of the array.
+ * @value: Value to search for.
+ * Return: Index of the value if found, otherwise -1.
+ */
 int linear_search(int *array, size_t size, int value)
 {
     size_t i = size;
@@ -7,6 +14,7 @@ int linear_search(int *array, size_t size, int value)
         return (-1);
     for (i = 0; i < size; i++)
     {
+        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
         if (array[i] == value)
             return (i);
     }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,12 +1,13 @@
 #include "search_algos.h"
 
-int binary_search(int *array, size_t size, int value);
-int binary_search_helper(int *array, size_t beginning, size_t end, int value);
-int binary_search(int *array, size_t size, int value)
-{
-    return (binary_search_helper(array, 0, size - 1, value));
-}
-
+/**
+ * binary_search_helper - Recursive helper function for binary search.
+ * @array: Pointer to the sorted array to search.
+ * @beginning: Index of the beginning of the subarray.
+ * @end: Index of the end of the subarray.
+ * @value: Value to search for.
+ * Return: Index of the value if found, otherwise -1.
+ */
 int binary_search_helper(int *array, size_t beginning, size_t end, int value)
 {
     size_t current = (beginning + end) / 2;
@@ -32,4 +33,16 @@ int binary_search_helper(int *array, size_t beginning, size_t end, int value)
         return (binary_search_helper(array, beginning, (current - 1), value));
 
     return (-1);
+}
+
+/**
+ * binary_search - Searches for a value in a sorted array using binary search.
+ * @array: Pointer to the sorted array to search.
+ * @size: Size of the array.
+ * @value: Value to search for.
+ * Return: Index of the value if found, otherwise -1.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+    return (binary_search_helper(array, 0, size - 1, value));
 }
