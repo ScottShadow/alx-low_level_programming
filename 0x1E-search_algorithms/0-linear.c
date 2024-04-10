675cc4b33148a231b8db3589a6a7e37445ba5931
Scott Rudaseswa <rudaseswascottmc@gmail.com>
2024-04-10 11:58:52 +0200

    codestyle fixes

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,15 +8,15 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i = size;
+	size_t i = size;
 
-    if (array == NULL)
-        return (-1);
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
-    }
-    return (-1);
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,29 +10,29 @@
  */
 int binary_search_helper(int *array, size_t beginning, size_t end, int value)
 {
-    size_t current = (beginning + end) / 2;
-    size_t i = 0;
-    if (array == NULL)
-        return (-1);
+	size_t current = (beginning + end) / 2;
+	size_t i = 0;
+	if (array == NULL)
+		return (-1);
 
-    printf("Searching in array: ");
-    for (i = beginning; i <= end; i++)
-        if (i != beginning)
-            printf(", %d", array[i]);
-        else
-            printf("%d", array[i]);
-    printf("\n");
-    if (array[current] == value)
-        return (current);
-    if (beginning == end)
-        return (-1);
+	printf("Searching in array: ");
+	for (i = beginning; i <= end; i++)
+		if (i != beginning)
+			printf(", %d", array[i]);
+		else
+			printf("%d", array[i]);
+	printf("\n");
+	if (array[current] == value)
+		return (current);
+	if (beginning == end)
+		return (-1);
 
-    if (array[current] < value)
-        return (binary_search_helper(array, (current + 1), end, value));
-    else if (array[current] > value)
-        return (binary_search_helper(array, beginning, (current - 1), value));
+	if (array[current] < value)
+		return (binary_search_helper(array, (current + 1), end, value));
+	else if (array[current] > value)
+		return (binary_search_helper(array, beginning, (current - 1), value));
 
-    return (-1);
+	return (-1);
 }
 
 /**
@@ -44,5 +44,5 @@ int binary_search_helper(int *array, size_t beginning, size_t end, int value)
  */
 int binary_search(int *array, size_t size, int value)
 {
-    return (binary_search_helper(array, 0, size - 1, value));
+	return (binary_search_helper(array, 0, size - 1, value));
 }
